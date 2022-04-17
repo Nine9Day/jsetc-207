c4e5653b2aba58d505ebf11efeeac7eef94a53bd
Nine9Day <853057112@qq.com>
2022-04-17 08:28:20 -0700

    five print

--- 1.c
+++ 1.c
@@ -3,7 +3,8 @@ int main(int argc, char const *argv[])
 {
     printf("hello jsetc\n");
     printf("hello nanjin\n");
-    printf("hello world\n");
-    printf("hello world\n");
+    printf("hello world1\n");
+    printf("hello world2\n");
+    printf("hello world3\n");
     return 0;
 }
